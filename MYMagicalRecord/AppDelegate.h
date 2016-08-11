1e705b6394ae9369f6f77ae40b399a483e9ceb73
zhangyoubo <13818956024@163.com>
2016-08-11 10:53:47 +0800

    Initial Commit

---
+++ MYMagicalRecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYMagicalRecord
+//
+//  Created by 张友波 on 16/8/11.
+//  Copyright © 2016年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYMagicalRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYMagicalRecord
+//
+//  Created by 张友波 on 16/8/11.
+//  Copyright © 2016年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
